381279eb103ba63da53026e6dace9a2f193eb364
김지은 <jxixeun@gmail.com>
2023-05-16 17:05:22 +0900

    :zap: [Feat] Token 사용 과정에서 발생하는 예외를 처리할 AuthenticationException 생성

---
+++ src/main/java/com/app/global/error/exception/AuthenticationException.java
@@ -0,0 +1,9 @@
+package com.app.global.error.exception;
+
+import com.app.global.error.ErrorCode;
+
+public class AuthenticationException extends BusinessException{
+    public AuthenticationException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
