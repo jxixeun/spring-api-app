e83f5185f1e5ac5aa4e500d3352c627297847524
김지은 <jxixeun@gmail.com>
2023-05-23 16:30:19 +0900

    :zap: [Fix] 기존 로직을 @MemberInfo 어노테이션으로 변경

--- src/main/java/com/app/api/member/controller/MemberInfoController.java
+++ src/main/java/com/app/api/member/controller/MemberInfoController.java
@@ -3,6 +3,8 @@ package com.app.api.member.controller;
 import com.app.api.member.dto.MemberInfoResponseDto;
 import com.app.api.member.service.MemberInfoService;
 import com.app.global.jwt.service.TokenManager;
+import com.app.global.resolver.memberinfo.MemberInfo;
+import com.app.global.resolver.memberinfo.MemberInfoDto;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +21,8 @@ public class MemberInfoController {
     private final TokenManager tokenManager;
 
     @GetMapping("/info")
-    public ResponseEntity<MemberInfoResponseDto> getMemberInfo(@RequestHeader("Authorization") String authorizationHeader){
-        String accessToken = authorizationHeader.split(" ")[1];
-        Claims tokenClaims = tokenManager.getTokenClaims(accessToken);
-        Long memberId = Long.valueOf((Integer) tokenClaims.get("memberId"));
+    public ResponseEntity<MemberInfoResponseDto> getMemberInfo(@MemberInfo MemberInfoDto memberInfoDto){
+        Long memberId = memberInfoDto.getMemberId();
         MemberInfoResponseDto memberInfoResponseDto = memberInfoService.getMemberInfo(memberId);
         return ResponseEntity.ok(memberInfoResponseDto);
     }
