33f5cbb2073ebb48c14bd5644886eb4b31807f76
김지은 <jxixeun@gmail.com>
2023-05-23 16:10:43 +0900

    :zap: [Add] 인터셉터를 Bean으로 등록

--- src/main/java/com/app/global/config/web/WebConfig.java
+++ src/main/java/com/app/global/config/web/WebConfig.java
@@ -1,12 +1,18 @@
 package com.app.global.config.web;
 
+import com.app.global.intercepter.AuthenticationIntercepter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final AuthenticationIntercepter authenticationIntercepter;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
@@ -20,5 +26,15 @@ public class WebConfig implements WebMvcConfigurer {
                         HttpMethod.OPTIONS.name()
                 )
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationIntercepter)
+                .order(1)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/oauth/login", "/api/access-token/issue",
+                        "/api/logout/",
+                        "/api/health");
     }
 }
