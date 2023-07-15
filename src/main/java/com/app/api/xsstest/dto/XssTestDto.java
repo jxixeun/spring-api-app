bc3f1ad2ebe00e36e7b293611e51a356704b4e5e
김지은 <jxixeun@gmail.com>
2023-07-16 03:13:16 +0900

    :sparkles: [Feat] XSS 방어 테스트용 컨트롤러와 Dto 생성

---
+++ src/main/java/com/app/api/xsstest/dto/XssTestDto.java
@@ -0,0 +1,12 @@
+package com.app.api.xsstest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class XssTestDto {
+    private String data;
+}
